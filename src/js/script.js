c9fc10f88d8c8e21010dac602d94d9697a009181
Daria Dorna <perelka4@onet.pl>
2023-06-26 16:08:43 +0200

    Second task bug

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,51 @@
-{
-  ('use strict');
 
-  const templates = {
-    bookLink: Handlebars.compile(document.querySelector('#template-book').innerHTML),
-  };
+('use strict');
 
-  function render() {
+const templates = {
+  bookLink: Handlebars.compile(document.querySelector('#template-book').innerHTML),
+};
 
-    for (let bookId in dataSource.books) {
-      bookId = dataSource.books[bookId];
-      const generatedHTML = templates.bookLink({
-        name: bookId.name,
-        price: bookId.price,
-        image: bookId.image,
-        rating: bookId.rating,
+function render() {
 
-      });
-      console.log('generatedHTML:', generatedHTML);
+  for (let bookId in dataSource.books) {
+    bookId = dataSource.books[bookId];
+    const generatedHTML = templates.bookLink({
+      name: bookId.name,
+      price: bookId.price,
+      image: bookId.image,
+      rating: bookId.rating,
+
+    });
 
 
-      const listOfBook = document.querySelector('.books-panel .books-list');
+    const listOfBook = document.querySelector('.books-panel .books-list');
 
-      const elem = utils.createDOMFromHTML(generatedHTML);
-      console.log('elem:', elem);
-      listOfBook.appendChild(elem);
-    }
+    const elem = utils.createDOMFromHTML(generatedHTML);
+
+    listOfBook.appendChild(elem);
   }
 
-
-  render();
 }
+
+
+const favoriteBooks = [];
+console.log(favoriteBooks);
+
+function initActions() {
+  const listOfBook = document.querySelector('.books-panel .books-list');
+
+  const booksImages = listOfBook.querySelectorAll('.book .book_image');
+  console.log(booksImages);
+
+
+  for (let i = 0; i < booksImages.length; i++) {
+    const clickedElement = this;
+    clickedElement.addEventListener('dblclick', function (event) {
+      event.preventDefault();
+      clickedElement.classList.add('favorite');
+    });
+  }
+}
+
+render();
+initActions();
